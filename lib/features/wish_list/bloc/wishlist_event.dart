fefdd231fbfb0ddb76883b24168a20500a0e5a62
dev-ravn <velmurugan1211r@gmail.com>
2024-05-21 17:16:30 +0530

    Authentication api integration

--- lib/features/wish_list/bloc/wishlist_event.dart
+++ lib/features/wish_list/bloc/wishlist_event.dart
@@ -2,3 +2,7 @@ part of 'wishlist_bloc.dart';
 
 @immutable
 sealed class WishlistEvent {}
+
+class WishlistInitialEvent extends WishlistEvent {}
+
+class WishlistRemoveEvent extends WishlistEvent {}
